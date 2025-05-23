77cca45abe2d96fbe5748fc403d8e553f7a0619a
Elias.Egel <elias.egelrid@gmail.com>
2025-05-23 09:25:51 +0200

    added team entity and gave it proper fields

---
+++ pokedex-backend/src/main/java/org/example/pokedexbackend/model/Team.java
@@ -0,0 +1,63 @@
+package org.example.pokedexbackend.model;
+
+
+import jakarta.persistence.*
+
+import java.util.ArrayList;
+
+@Entity
+@Table(name = "teams")
+public class Team {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @OneToMany(mappedBy = "team", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<TeamPokemon> pokemons = new ArrayList<>();
+
+    public Team(Long id, String name, User user, List<TeamPokemon> pokemons) {
+        this.id = id;
+        this.name = name;
+        this.user = user;
+        this.pokemons = pokemons;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<TeamPokemon> getPokemons() {
+        return pokemons;
+    }
+
+    public void setPokemons(List<TeamPokemon> pokemons) {
+        this.pokemons = pokemons;
+    }
+}
