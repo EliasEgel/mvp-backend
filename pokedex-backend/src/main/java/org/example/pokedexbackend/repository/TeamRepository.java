1ce9546fb31acd5a805e4daff7f9f7d9d95c3ee8
Elias.Egel <elias.egelrid@gmail.com>
2025-05-23 10:18:45 +0200

    annotated the teamrepository correctly

--- pokedex-backend/src/main/java/org/example/pokedexbackend/repository/TeamRepository.java
+++ pokedex-backend/src/main/java/org/example/pokedexbackend/repository/TeamRepository.java
@@ -2,6 +2,8 @@ package org.example.pokedexbackend.repository;
 
 import org.example.pokedexbackend.model.Team;
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TeamRepository extends ListCrudRepository<Team, Long> {
 }
